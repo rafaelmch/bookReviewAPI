d07670a2595acdc4822d41d7d45a7dad60846648
Rafael Hassegawa <rafaelmch@gmail.com>
2025-04-30 14:53:23 -0300

    Add Springdoc OpenAPI dependency and Swagger UI configuration

--- bookReview/src/main/java/com/bookReview/bookReview/BookReviewApplication.java
+++ bookReview/src/main/java/com/bookReview/bookReview/BookReviewApplication.java
@@ -1,9 +1,22 @@
 package com.bookReview.bookReview;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title       = "Book Review API",
+				version     = "1.0",
+				description = "CRUD + filtering for books",
+				contact     = @Contact(name="Raf", email="rafaelmch@gmail.com")
+		),
+		servers = @Server(url = "http://localhost:8081")
+)
 public class BookReviewApplication {
 
 	public static void main(String[] args) {
